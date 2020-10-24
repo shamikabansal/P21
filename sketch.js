2e68c1515b0070525af085da79832327edb59808
shamikabansal <71285046+shamikabansal@users.noreply.github.com>
2020-10-24 11:41:00 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var bullet, wall;
+var speed, weight, thickness;
+
+
+function setup() {
+  createCanvas(1600,400);
+ 
+  speed = random(223,321);
+  weight = random(30,52);
+  thickness = random(22,83);
+ 
+ 
+  bullet = createSprite(50,200,75,15);
+  bullet.shapeColor = color(0,0,0);
+  bullet.velocityX = speed;
+  
+  
+  wall = createSprite(1200,200,thickness,200);
+  wall.shapeColor = color(80,80,80);
+}
+
+function draw () {
+ 
+  background(255,255,255); 
+ 
+  wall.display(); 
+  bullet.display();
+  
+  if (hasCollided(bullet, wall)) {
+    bullet.velocityX = 0;
+    
+    var  damage = (0.5 * weight * speed * speed) / (thickness * thickness * thickness);
+    
+    if (damage < 10 && damage > 0 ) {
+     wall.shapeColor = color(0,255,0);
+   
+    }
+    if (damage > 10 ) {
+     wall.shapeColor = color(255,0,0);
+   
+    }
+    
+  }
+  
+  drawSprites();
+}
+
+function hasCollided(bullet, wall){
+  bulletRightEdge = bullet.x +bullet.width;
+  wallleftEdge= wall.x;
+  if (bulletRightEdge>=wallleftEdge){
+    return true
+  }
+  return false;
+
+}
